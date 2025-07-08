fe5c3b7a94dd3bb30c269cb087abf270883f72e6
oct <sangwoo1248@gmail.com>
2025-07-09 00:35:03 +0900

    ver11

---
+++ content.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  document.querySelectorAll(".scroll-fade-in").forEach((el) => {
+    observer.observe(el);
+  });
+});
---
+++ customer.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".faq-question").forEach((button) => {
+  button.addEventListener("click", () => {
+    const item = button.parentElement;
+    item.classList.toggle("active");
+  });
+});
