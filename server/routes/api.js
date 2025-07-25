efd6b2f56ed6a0047974a6d4c70cc6c96b6907a4
oct <sangwoo1248@gmail.com>
2025-07-26 00:01:03 +0900

    ver 17

--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example GET route
-router.get('/', (req, res) => {
-  res.json({ message: 'API root is working' });
-});
-
-module.exports = router;
